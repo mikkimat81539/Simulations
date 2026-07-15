5aa104dbc6773e6a080f4bbb4958784fdae363f0
mikkimat81539 <mikkimat81539@gmail.com>
2026-07-15 18:25:53 +0100

    ball moving along the y axis

--- Ball_Physics/main.py
+++ Ball_Physics/main.py
@@ -7,7 +7,7 @@ class Ball_Object:
 	def __init__(self, x_pos, y_pos, radius, color):
 		self.x_pos = x_pos
 		self.y_pos = y_pos
-		self.center = (self.x_pos, self.y_pos)
+		self.center = pygame.math.Vector2(self.x_pos, self.y_pos)
 		self.radius = radius
 		self.color = color
 		self.speed = 5
@@ -18,17 +18,24 @@ class Ball_Object:
 		pygame.draw.circle(surface, self.color, self.center, self.radius)
 
 	def moveObject(self):
+		# When spacebar is pressed ball needs to go up in the y_pos
 		key = pygame.key.get_pressed()
 
 		if key[pygame.K_SPACE]:
-			print("pressed")
+			self.center[1] -= self.y_pos
+
+# INITIALIZE
+pygame.init()
+
+# CLOCK
+clock = pygame.time.Clock()
 
 # SCREEN
 screen = pygame.display.set_mode((500, 500))
 pygame.display.set_caption("Bouncing Ball")
 
 # BALL
-ball = Ball_Object(100, 100, 10, "black")
+ball = Ball_Object(250, 350, 10, "black")
 
 # GAME LOOP
 running = True
@@ -38,16 +45,17 @@ while running:
 		if event.type == pygame.QUIT:
 			running = False
 
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_SPACE:
-				print("pressed")
-
 	
 	screen.fill("white")
+
+	# MOVEMENT
+	ball.moveObject()
 
 	# DRAW
 	ball.drawObject(screen)
 
 	pygame.display.flip()
+
+	clock.tick(40)
 
 pygame.quit()
