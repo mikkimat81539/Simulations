70e78167a46e8a76494df81cf5075aedb820fd2f
mikkimat81539 <mikkimat81539@gmail.com>
2026-07-16 18:40:11 +0100

    displayed surface on screen

--- Ball_Physics/multiball.py
+++ Ball_Physics/multiball.py
@@ -10,16 +10,40 @@ import pygame
 	# when ball is less than 1, remove ball
 
 # BONUS surface: spinning surface and ball is going to roll based on direction of surface spinning
-	# Use Math
 
+
+# INITALIZE
 pygame.init()
 
+# SURFACE CLASS
+class Ball_Surface:
+	def __init__(self, x_pos, y_pos, width, height, color):
+		self.x_pos = x_pos
+		self.y_pos = y_pos
+		self.width = width
+		self.height = height
+		self.color = color
+
+		self.surface = pygame.Surface((self.width, self.height))
+
+	def draw_surface(self, surface):
+		self.surface.fill(self.color)
+		surface.blit(self.surface, (self.x_pos, self.y_pos))
+
+# BALL CLASS
+class Ball_Object:
+	pass
+
+# SCREEN
 screen_w, screen_h = 800, 600
 
 screen = pygame.display.set_mode((screen_w, screen_h))
 pygame.display.set_caption("Multi-Ball")
 
-# Game Loop
+# OBJECTS
+surface1 = Ball_Surface(10, 10, 100, 100, "pink")
+
+# GAME LOOP
 running = True
 
 while running:
@@ -28,6 +52,9 @@ while running:
 			running = False
 
 	screen.fill("white")
+
+	# DRAW
+	surface1.draw_surface(screen)
 
 	pygame.display.flip()
 
