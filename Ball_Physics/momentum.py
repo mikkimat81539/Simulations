025a90cc68351ab791bde3773d908ddd81f7d0d4
mikkimat81539 <mikkimat81539@gmail.com>
2026-07-19 18:27:30 +0100

    applied collision and momentum

--- Ball_Physics/momentum.py
+++ Ball_Physics/momentum.py
@@ -1,11 +1,12 @@
 # create two balls -- DONE
 # Have balls collide with each other
 	# ball1 will have a momentum and when it collides with ball2 the momentum will be transferred to ball2
-		# when spacebar is pressed than move ball1 when ball2 hits wall stop
+		# when spacebar is pressed than move ball1 -- DONE
+		# when ball2 hits wall stop
 			# apply charge when spacebar is held
-		# Have different speeds
+			# Have different speeds
 
-import pygame
+import pygame, math
 
 pygame.init()
 
@@ -38,10 +39,30 @@ screen = pygame.display.set_mode((500, 500))
 pygame.display.set_caption("Momentum")
 
 # BALL
-ball1 = Ball(50, 250, 30, "black", 7, 10)
-ball2 = Ball(250, 250, 30, "red", 7, 10)
+ball1 = Ball(50, 250, 30, "black", 5, 10)
+ball2 = Ball(250, 250, 30, "red", 0, 10)
 
-activate = False
+
+# COLLISION
+def collision(ball1, ball2):
+	total_x = (ball2.center[0] - ball1.center[0]) ** 2
+	total_y = (ball2.center[1] - ball1.center[1]) ** 2
+
+	total_radii = ball2.radius + ball1.radius
+
+	d = int(math.sqrt(total_x + total_y))
+
+	if d < total_radii and ball1.velocity != 0:
+		ball1_p = ball1.mass * ball1.velocity # Ball 1 momentum
+
+		ball1.velocity = 0
+
+		ball2.velocity = ball1_p // ball2.mass
+
+		# print(ball1.velocity, ball2.velocity)
+
+
+	return ball1.velocity, ball2.velocity
 
 # GAME LOOP
 running = True
@@ -61,10 +82,16 @@ while running:
 
 	screen.fill("white")
 
+	# COLLISION
+	ball1.velocity, ball2.velocity = collision(ball1, ball2)
+
+	if ball2.velocity != 0:
+		ball2.move_object()
+
 	# MOVEMENT
 	if ball1.activate == True:
 		ball1.move_object()
-	
+
 	# DRAW
 	ball1.draw_object(screen)
 	ball2.draw_object(screen)
